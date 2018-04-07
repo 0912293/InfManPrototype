cad2dff2ef7e23dbf84baa66c1f9294ffff56956
Jamal <JamalG94>
2018-04-07 17:20:32 +0200

    Created the database connection, resultset querying and barchart plotting

---
+++ app/src/main/java/com/datacorner/app/prototype/FirstExample.java
@@ -0,0 +1,127 @@
+package com.datacorner.app.prototype;// Use the JDBC driver
+import android.os.AsyncTask;
+import android.os.StrictMode;
+import android.util.Log;
+
+import java.sql.*;
+
+//public class DatabaseConnector{
+
+//    // Connect to your database.
+//    // Replace server name, username, and password with your credentials
+//    Connection connection = null;
+//
+//
+//    @Override
+//    protected Void doInBackground(String... strings) {
+//        String connectionString =
+//                "jdbc:jtds:sqlserver://localhost:1433;"
+//                        + "database=INFMAN;"
+//                        + "user=androidstudio;"
+//                        + "password=coolboy123;"
+//                        + "encrypt=true;"
+//                        + "trustServerCertificate=false;"
+//                        + "hostNameInCertificate=*.database.windows.net;"
+//                        + "loginTimeout=30;";
+//
+//
+//
+//        try {
+//            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+//
+//            connection = DriverManager.getConnection(connectionString);
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+//
+//    public ResultSet executeQuery() {
+//        ResultSet rs = null;
+//        try{
+//            Statement stmt = connection.createStatement();
+//            rs = stmt.executeQuery("SELECT * FROM PostFacts;");
+//        }
+//        catch(Exception e){
+//
+//        }
+//        return rs;
+//    }
+
+//}
+import java.sql.*;
+
+public class FirstExample {
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost/INFMAN";
+
+    //  Database credentials
+    static final String USER = "infman2";
+    static final String PASS = "Yk7z3!cw2!p0";
+
+    public ResultSet executeQuery() {
+        String ConnURL = null;
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        try{
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                    .permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            ConnURL = "jdbc:jtds:sqlserver://" + "den1.mssql5.gear.host" + ";"
+                    + "databaseName=" + "infman2" + ";user=" + USER + ";password="
+                    + PASS + ";";
+
+            //STEP 2: Register JDBC driver
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+
+            //STEP 3: Open a connection
+            Log.d("Connecting", "Connecting to database...");
+            conn = DriverManager.getConnection(ConnURL);
+
+            //STEP 4: Execute a query
+            Log.d("Creating", "Creating statement...");
+            stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT TOP(100) * FROM PostFacts";
+            rs = stmt.executeQuery(sql);
+
+            //STEP 5: Extract data from result set
+//            while(rs.next()){
+//                //Retrieve by column name
+//                int PostLikes  = rs.getInt("PostLikes");
+//
+//
+//                //Display values
+//                Log.d("It is", "ID: " + PostLikes);
+//            }
+            //STEP 6: Clean-up environment
+            //rs.close();
+            //stmt.close();
+            //conn.close();
+        }catch(SQLException se){
+            //Handle errors for JDBC
+            se.printStackTrace();
+        }catch(Exception e){
+            //Handle errors for Class.forName
+            e.printStackTrace();
+//        }finally{
+//            //finally block used to close resources
+//            try{
+//                if(stmt!=null)
+//                    stmt.close();
+//            }catch(SQLException se2){
+//            }// nothing we can do
+//            try{
+//                if(conn!=null)
+//                    conn.close();
+//            }catch(SQLException se){
+//                se.printStackTrace();
+//            }//end finally try
+        }//end try
+        Log.d("Bye", "Goodbye :)!");
+        return rs;//end main
+    }
+}//end FirstExample
--- app/src/main/java/com/datacorner/app/prototype/MainActivity.java
+++ app/src/main/java/com/datacorner/app/prototype/MainActivity.java
@@ -2,16 +2,31 @@ package com.datacorner.app.prototype;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.HorizontalBarChart;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.transform.Result;
 
 public class MainActivity  extends AppCompatActivity {
 
@@ -19,51 +34,137 @@ public class MainActivity  extends AppCompatActivity {
     ArrayList<BarEntry> entries = new ArrayList<>();
     ArrayList<String> labels = new ArrayList<>();
     BarDataSet dataset;
+    BarChart barChart;
+    BarData data = new BarData();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BarChart barChart = (BarChart) findViewById(R.id.chart);
 
-        chartData();
+        FirstExample firstExample = new FirstExample();
+        ResultSet rs = firstExample.executeQuery();
+
+        barChart = (BarChart) findViewById(R.id.chart);
 
 
-        XAxis xAxis = barChart.getXAxis();
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-        xAxis.setDrawGridLines(true);
 
-        BarData data = new BarData(dataset); // initialize BarData
-        barChart.setData(data); //set data into chart
-        dataset.setColors(Color.CYAN); // set the color
-        dataset.setValueTextSize(15f);
+        chartData(rs);
 
-        Legend l = barChart.getLegend();
-        l.setEnabled(true);
-        l.setFormSize(10f);
-        l.setTextSize(15.8f);
-        l.setWordWrapEnabled(true);
 
-        barChart.setVisibleXRangeMaximum(1.5f);
 
-        barChart.animateY(1000);
-//        barChart.setDescription("a");  // set the description rechtsonder
+
+
     }
 
     //retrieve and put data in chart
-    public void chartData(){
-//        DataFactory dataFactory = new DataFactory();
-//        IData<BarEntry> query1 = dataFactory.getQuery("BAR");
-//        query1.query(context);
-//        entries = query1.getData();
-//        labels = query1.getLabel();
-        for(int i = 0; i<=10; i++)
-        {
-            entries.add(new BarEntry(i,i));
-            labels.add(Integer.toString(i));
-        }
+    public void chartData(ResultSet rs){
 
-        dataset = new BarDataSet(entries, "dummy");
+        String companyName;
+        int postLikes;
+        int postComments;
+
+        Map<String, Integer> likeDictionary = new HashMap<>();
+        Map<String, Integer> commentDictionary = new HashMap<>();
+
+        try {
+            while(rs.next()){
+                companyName = rs.getString("CompanyName");
+                postLikes = rs.getInt("PostLikes");
+                postComments = rs.getInt("PostComments");
+
+                //Retrieve by column name
+                if(likeDictionary.get(companyName) == null){
+                    likeDictionary.put(companyName, postLikes);
+                }
+                else{
+                    int a = likeDictionary.get(rs.getString("CompanyName"));
+                    likeDictionary.replace(companyName, a + postLikes);
+                }
+
+                if(commentDictionary.get(rs.getString("CompanyName")) == null){
+                    commentDictionary.put(rs.getString("CompanyName"), rs.getInt("PostComments"));
+                }
+                else{
+                    int a = commentDictionary.get(rs.getString("CompanyName"));
+                    commentDictionary.replace(companyName, a + postComments);
+                }
+
+//                PostLikes  = rs.getInt("PostLikes");
+//                Brand = rs.getString("CompanyName");
+//                Comments = rs.getInt("PostComments");
+            }
+            int i = 0;
+            int j = 1;
+            for (Map.Entry<String, Integer> entry : likeDictionary.entrySet())
+            {
+
+                entries.add(new BarEntry(i, entry.getValue()));
+                labels.add(entry.getKey()+" Likes");
+                i = i+2;
+            }
+            for (Map.Entry<String, Integer> entry : commentDictionary.entrySet())
+            {
+                entries.add(new BarEntry(j, entry.getValue()));
+                labels.add(entry.getKey()+" Comments");
+                j = j+2;
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        createChart();
+
+
+    }
+    public void createChart(){
+        ArrayList<String> lobels = new ArrayList<>(labels);
+        BarDataSet dataSet = new BarDataSet(entries, "Tenses");
+        dataSet.setDrawValues(true);
+
+        // Create a data object from the dataSet
+        BarData data = new BarData(dataSet);
+        // Format data as percentage
+//        data.setValueFormatter(new PercentFormatter());
+
+        // Make the chart use the acquired data
+        barChart.setData(data);
+
+        // Create the labels for the bars
+//        final ArrayList<String> xVals = new ArrayList<>();
+//        xVals.add("Present");
+//        xVals.add("Pres. Continuous");
+//        xVals.add("Simple Past");
+//        xVals.add("Past Perfect");
+
+
+        // Display labels for bars
+        barChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(lobels));
+
+        // Set the maximum value that can be taken by the bars
+        //barChart.getAxisLeft().setAxisMaximum(100);
+
+        // Bars are sliding in from left to right
+        barChart.animateXY(1000, 1000);
+        // Display scores inside the bars
+        barChart.setDrawValueAboveBar(false);
+
+        // Hide grid lines
+        barChart.getAxisLeft().setEnabled(false);
+        barChart.getAxisRight().setEnabled(false);
+        // Hide graph description
+        barChart.getDescription().setEnabled(false);
+        // Hide graph legend
+        barChart.getLegend().setEnabled(false);
+
+        // Design
+        dataSet.setColors(ColorTemplate.VORDIPLOM_COLORS);
+        data.setValueTextSize(13f);
+        data.setValueTextColor(Color.DKGRAY);
+
+        barChart.invalidate();
+
     }
 }
